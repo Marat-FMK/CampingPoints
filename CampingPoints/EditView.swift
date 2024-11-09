ab60e450af0910f496af9c02ac2503151f2f4fbe
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-11-09 21:55:38 +0300

    fix view

--- CampingPoints/EditView.swift
+++ CampingPoints/EditView.swift
@@ -10,18 +10,20 @@ import Foundation
 
 struct EditView: View {
     
-    enum LoadingState {
-        case loading, loaded, failed // состояния сетевого запроса
-    }
+//    enum LoadingState {
+//        case loading, loaded, failed // состояния сетевого запроса
+//    }
     
     @Environment(\.dismiss) var dismiss
     var location: Location
 
+    @State private var viewModel = ViewModel()
+    
     @State private var name: String
     @State private var description: String
     var onSave: (Location) -> Void
     
-    @State private var loadingState = LoadingState.loading
+    @State private var loadingState = viewModel.loadingState
     @State private var pages = [Page]()
 
     var body: some View {
@@ -60,7 +62,7 @@ struct EditView: View {
                 }
             }
             .task {
-                await fetchNearbyPlaces()
+                await viewModel.fetchNearbyPlaces()
             }
         }
     }
@@ -72,28 +74,7 @@ struct EditView: View {
                 _description = State(initialValue: location.description)
             }
     
-    func fetchNearbyPlaces() async {
-        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.latitude)%7C\(location.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
-
-        guard let url = URL(string: urlString) else {
-            print("Bad URL: \(urlString)")
-            return
-        }
-
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-
-            // we got some data back!
-            let items = try JSONDecoder().decode(Result.self, from: data)
-
-            // success – convert the array values to our pages array
-            pages = items.query.pages.values.sorted()
-            loadingState = .loaded
-        } catch {
-            // if we're still here it means the request failed somehow
-            loadingState = .failed
-        }
-    }
+    
     
 }
 
